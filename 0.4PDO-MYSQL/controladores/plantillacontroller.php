bff42b80e738ce112f3f3eb8fdb009af623a94d6
jimenaciros30 <47190257+jimenaciros30@users.noreply.github.com>
2020-10-20 14:51:38 -0500

    Add files via upload

---
+++ 0.4PDO-MYSQL/controladores/plantillacontroller.php
@@ -0,0 +1,14 @@
+<?php
+#La pagina es llamada a traves del controlador  y la envia al index
+class ControladorPlantilla{
+	#Llamada a la plantila
+	#-------------------------------------------------------------
+	public function ctrTraerPlantilla(){
+
+		include "vistas/template.php"; #include es como echo pero muestra el archivo
+
+	}
+
+}
+
+?>
